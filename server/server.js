cdedf65c1a3a4b2929c5941f6be9b3ebf8cd7888
Arnold Agus <Arnold Agus>
2016-05-03 19:24:06 +1000

    Firsdt commit for our derpy project

---
+++ server/server.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static('../app/'));
+
+
+console.log(__dirname + "../app/");
+app.get('/', function (req, res) {
+   res.sendFile("index.html");
+})
+
+
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})
